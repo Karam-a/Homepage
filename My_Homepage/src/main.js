3b9e7945d514f71dd74eb31e9a04440d10be2715
Karam-a <81566216+Karam-a@users.noreply.github.com>
2024-12-04 19:01:09 +0100

    Added Experience and Navigation.

--- My_Homepage/src/main.js
+++ My_Homepage/src/main.js
@@ -12,3 +12,7 @@ app.use(createPinia())
 app.use(router)
 
 app.mount('#app')
+
+<template>
+    
+</template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class',
   content: [
     "./index.html",
     "./src/Homepage/*.{vue,js,ts,jsx,tsx}",
