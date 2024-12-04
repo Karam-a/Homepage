d8964cc1d10d9b12e2096dbee3d7e10253e72a0e
Karam-a <81566216+Karam-a@users.noreply.github.com>
2024-12-04 17:27:42 +0100

    Added Tailwind CSS & Vue to project repo

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/Homepage/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
